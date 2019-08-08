844f8d81ebfc0e9812aecaa8b1133909aff79e83
Muzi <muzixaba@gmail.com>
2019-08-08 13:02:19 +0200

    added some stuff

--- pandas/pandas_stuff.py
+++ pandas/pandas_stuff.py
@@ -2,9 +2,28 @@
 # A collection of pandas tips and tricks
 #==================================================
 
+# Delete a column
+del df['col_name']
+
+# Change column data type
+df['col_name'] = df['col_name'].astype('int64')
+
 # Return df by column types
-df = df.select_dtypes(exclude=['object','int64','float64','bool','datetime64','timedelta[ns]','category'])
+df.select_dtypes(exclude=['object','int64','float64','bool','datetime64','timedelta[ns]','category'])
 
 # Finds sum of nans in each column
 df.isna().sum()
 
+# Count non-NA cells for each column or row.
+df.count()
+
+
+#==========================================
+# Combining DataFrames
+#=========================================
+
+# Merged 2 dfs using different columns
+new_df = pd.merge(left=df1, right=df2, how='inner', left_on='df1_col', right_on='df2_col')
+
+# merge 2 dfs using their indecies
+df3 = pd.merge(df1, df2, right_index=True, left_index=True)
