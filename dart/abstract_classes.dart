60e43118930d1dc10ee320a7b3e8a9be05ff9982
Muzi <muzixaba@gmail.com>
2019-09-14 15:02:47 +0200

    more info on dart classes

--- dart/abstract_classes.dart
+++ dart/abstract_classes.dart
@@ -16,22 +16,31 @@ class PowerGrid {
   }
 }
 
-/// abstract class to classify types using their methods.
+/// abstract class to classify types using their methods & instance variables.
 /// if class has turnOn(), it's a PowerPlant
 abstract class PowerPlant {
+  int costOfEnergy;
   turnOn();
+}
+
+abstract class PlantSize {
+  int sizeOfPlant;
 }
 
 /// NewClass implements AbstractClass
 /// At minimum, it has to have the same methods as the AbstractClass
 class NuclearPlant implements PowerPlant {
+  //instance variables have to have the same name & type
+  int costOfEnergy;
   turnOn() {
     print("Nuclear Plant switched on");
   }
 }
 
-class SolarPlant implements PowerPlant {
-  
+// A class can implement multiple abstract classes
+class SolarPlant implements PowerPlant, PlantSize {
+  int costOfEnergy;
+  int sizeOfPlant;
   turnOn() {
     print("Solar plant switched on");
   }
