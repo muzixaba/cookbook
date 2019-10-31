f416e40633cd6cbd368102ed92b930070d9083e0
Muzi <muzixaba@gmail.com>
2019-10-31 13:02:23 +0200

    added editors folder

--- python/multiprocessing_basics.py
+++ python/multiprocessing_basics.py
@@ -1,8 +1,14 @@
 import multiprocessing as mp
-
+from loky import ProcessPoolExecutor
 # normal code
 res = map(greet, ['Sipho', 'Sindi'])
 
 # use worker pools
 pool = mp.Pool(2)
 res = pool.map(greet, ['Sipho', 'Sindi'])
+
+# use loky for cross platform consistancy
+executor = ProcessPoolExecutor(max_workers=2)
+res = executor.map(greet, ('Sipho', 'Sindi'))
+
+#TODO: Research joblib
