a28ac87a7a7b5ef255e885f7c774711509426872
Muzi <muzixaba@gmail.com>
2019-11-04 13:58:12 +0200

    updated multiprocessing

--- python/multiprocessing_basics.py
+++ python/multiprocessing_basics.py
@@ -12,3 +12,17 @@ executor = ProcessPoolExecutor(max_workers=2)
 res = executor.map(greet, ('Sipho', 'Sindi'))
 
 #TODO: Research joblib
+
+
+def run_in_parallel():
+    eight = len(ids_to_do)//8
+    quater = len(ids_to_do)//4
+    half = len(ids_to_do)//2
+
+    args = [ids_to_do[:eight],ids_to_do[eight:quater], 
+            ids_to_do[quater:(quater+eight)],ids_to_do[(quater+eight):half], 
+            ids_to_do[half:-(quater+eight)],ids_to_do[-(quater+eight):-quater], 
+            ids_to_do[-quater:-eight],ids_to_do[-eight:]]
+    
+    pool = multiprocessing.Pool(processes=len(args))
+    pool.map(export_images2, args)
