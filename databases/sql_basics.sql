62e388eefd711dd5368a2f36e85bb150c0485a02
Muzi Xaba <muzi@explore-ai.net>
2020-09-29 14:25:55 +0200

    update databases/sql

---
+++ databases/sql_basics.sql
@@ -0,0 +1,34 @@
+-- Create a database
+
+
+-- Cteate a table inside a database
+
+
+-- Insert Single Record
+
+
+-- Insert Multiple Records
+
+
+-- Search for records using sing table
+
+
+-- Search for records using multiple tables
+
+
+-- Search using wildcards ('%', '_')
+
+
+-- Joining Tables
+-- Join Types (Inner, Full, Left, Outer, Cross)
+/*
+SELECT column(s)
+FROM table1
+<join_type> JOIN table2
+ON <Join condition>
+*/
+
+-- Grouping Results (groupby)
+
+
+-- Calculated Fields
