b548dff6378fb9d80fac54ef7afd16d70f0f5e53
Muzi <muzixaba@gmail.com>
2021-05-29 10:36:52 +0200

    update vscode, drf_api, and react stuff

---
+++ javascript/react_basics.js
@@ -0,0 +1,17 @@
+// Installing nodejs
+/*
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/\
+install.sh | bash
+
+command -v nvm
+*/
+
+
+//Create react app
+mkdir project_name && cd project_name
+npx create-react-app app_name
+cd app_name
+npm start
+
+// install axios for making requests
+npm install axios
