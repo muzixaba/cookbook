18892c5b31eda796f7a29ac9fe0bf52a7b83433b
Muzi <muzixaba@gmail.com>
2022-10-15 08:23:45 +0200

    update doctest example

--- python/unittest_tests/doctest_basics.py
+++ python/unittest_tests/doctest_basics.py
@@ -1,5 +1,5 @@
 '''
-Doctests helps to run tests using your docstrings.
+Doctest helps to run tests using docstrings.
 You embed interactive python sessions and Doctest tests those.
 Can be run by calling doctest.testmod() inside of script.
 Also run at module-level `python -m doctest module_name.py -v`
@@ -9,17 +9,32 @@ def adder(a, b):
     '''
     Returns the sum of a and b
 
+    :param a: int
+    :param b: int
+    :return: int
+
     >>> adder(2, 3)
     5
+
+    >>> adder("2", 2)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
 
     >>> adder(2, "2")
     Traceback (most recent call last):
         ...
-    TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    TypeError: b must be an integer
     '''
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
     result = a + b
     return(result)
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)
+
+# print(adder("2", 2))
